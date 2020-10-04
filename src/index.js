d384c9146d3a7389aa27c8f9787bf5a59576fe81
Kadamir24 <amir.kaniyev@gmail.com>
2020-10-04 21:13:50 +0600

    feat: finish reverse task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,15 @@
 module.exports = function reverse (n) {
-  
+    if (n < 0) {
+        n = n * (-1);
+    }
+    strNum = n + '';
+    let newStr = '';
+    for (let i = strNum.length - 1; i >= 0; i--) {
+        if (i == strNum.length - 1 && strNum[i] == 0) {
+            continue
+        }
+        newStr += strNum[i];
+
+    }
+    return newStr;
 }
